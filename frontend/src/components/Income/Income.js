6ef24efac75a7bc6484a0f3a566db36da4ccb1bb
Neha Saleem Desai <nehasaleemdesai99@gmail.com>
2025-02-05 21:57:32 -0600

    Add React App to the repo

---
+++ frontend/src/components/Income/Income.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../../styles/Layouts";
+import { useGlobalContext } from "../../context/globalContext";
+import Form from "../Form/Form";
+
+function Income() {
+  const { addIncome, incomes, getIncomes } = useGlobalContext();
+
+  useEffect(() => {
+    getIncomes();
+  }, []);
+
+  return (
+    <IncomeStyled>
+      <InnerLayout>
+        <h1>Incomes</h1>
+
+        <div className="income-content">
+          <div className="form-container">
+            <Form />
+          </div>
+          <div className="incomes">
+            {incomes.map((income) => {
+              const { _id, title, amount, date, category, description } =
+                income;
+            })}
+          </div>
+        </div>
+      </InnerLayout>
+    </IncomeStyled>
+  );
+}
+
+const IncomeStyled = styled.div``;
+
+export default Income;
---
+++ frontend/src/components/IncomeItem/incomeItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function incomeItem() {
+  return (
+    <IncomeItemStyled>
+      <div className="icon"></div>
+    </IncomeItemStyled>
+  );
+}
+
+const IncomeItemStyled = styled.div``;
+
+export default incomeItem;
