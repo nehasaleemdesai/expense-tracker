d3a6d49b3bafc079801f0bf7292149375c0dbd23
Neha Saleem Desai <nehasaleemdesai99@gmail.com>
2025-03-04 22:59:10 -0600

    API changes in useContext

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,8 +1,8 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1";
-// const BASE_URL = "http://expensetracker-z3s665se.b4a.run";
+// const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://expensetracker-z3s665se.b4a.run/api/v1";
 
 const GlobalContext = React.createContext();
 
