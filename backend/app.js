6cf37ef50835a6a070627ece62c4d2ae009bdd62
Neha Saleem Desai <nehasaleemdesai99@gmail.com>
2025-03-06 22:45:35 -0600

    checking aPI issues

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 443;
 
 //middlewares
 app.use(express.json());
-// app.use(cors());
-app.use(cors({ origin: "*" }));
+app.use(cors());
+// app.use(cors({ origin: "*" }));
 
 //routes
 readdirSync("./routes").map((route) => {
